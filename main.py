b85f20f35d658b99ef2d0a80d9a43a08e1d6585f
student-tets <guess.the.student@gmail.com>
2024-11-07 13:42:40 +0300

    Описали функцию get_ris_data

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ def get_ris_data(path: str, col_name: str) -> any:
   :path: путь к файлу
   :col_name: название колонки
   """
-  pass
-
+  # прочитать файл с помощью функции open()
+  file = open(path, "r")
+  # создадим пустой список для хранения аннотаций
+  abs = []
+  # переберем строки в цикле for 
+  for line in file.readlines():
+    # разобьем строку по общему символу, предварительно убрав символ переноса строки "\n" 
+    splitted_line = line.strip("\n").split("  - ")
+    # находим строки с AB и добавляем аннотации в список abs
+    if col_name == splitted_line[0]:
+      abs.append(splitted_line[1])
+  # закрываем чтение файла
+  file.close()
+  return abs
