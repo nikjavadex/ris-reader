d88ab058791671c3c6431f9229ea0b192ccf5711
student-tets <guess.the.student@gmail.com>
2024-11-07 13:00:59 +0300

    описали документацию к функции get_ris_data

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 ## Модуль для импорта данных из ris-файла
 
-def get_ris_data(path, col_name):
+def get_ris_data(path: str, col_name: str) -> any:
+  """
+  Выгрузка колонки из ris-файла в ...
+
+  :path: путь к файлу
+  :col_name: название колонки
+  """
   pass
+
